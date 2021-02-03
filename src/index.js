642e4147ce247143e8efd11b361e8879450d9a69
mojpm <itsjoelpm@gmail.com>
2021-02-03 19:50:31 +0300

    add error boundary

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ErrorBoundary from './ErrorBoundary'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
