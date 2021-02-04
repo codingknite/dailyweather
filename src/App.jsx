f73f49ecf603273eca5b703e4dbe3f5054a18ab6
mojpm <itsjoelpm@gmail.com>
2021-02-04 19:41:33 +0300

    pass lifted state to highlights comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Weather from "./Weather";
 import Highlights from "./Highlights";
@@ -11,10 +11,21 @@ const Main = styled.main`
 `;
 
 export default function App() {
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    fetch("https://extreme-ip-lookup.com/json/")
+      .then((response) => response.json())
+      .then((data) => setCity(data.city))
+      .catch((error) => {
+        throw error;
+      });
+  }, []);
+
   return (
     <Main>
-      <Weather />
-      <Highlights />
+      <Weather city={city} />
+      <Highlights city={city} />
     </Main>
   );
 }
