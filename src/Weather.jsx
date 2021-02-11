070d3a4611a2b3e5d0401e4c16b6efa4e4cd7dc5
mojpm <itsjoelpm@gmail.com>
2021-02-11 11:36:34 +0300

    decompose Highlights component

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import styled from "styled-components";
 import WeatherDisplay from "./WeatherDisplay";
-import Highlights from "./Highlights";
+
+const Main = styled.main`
+  background: #edf2f4;
+  height: 100vh;
+  display: flex;
+`;
 
 const Section = styled.section`
   display: flex;
   flex-direction: column;
   width: 30vw;
+  height: 100vh;
   background-color: #002521;
   color: white;
 `;
 
 const Weather = ({ city, loading, error, weatherData, celcius }) => {
   return (
-    <Section>
+    <>
+      {/* <Section> */}
       <WeatherDisplay
         city={city}
         loading={loading}
@@ -21,7 +28,8 @@ const Weather = ({ city, loading, error, weatherData, celcius }) => {
         weatherData={weatherData}
         celcius={celcius}
       />
-    </Section>
+      {/* </Section> */}
+    </>
   );
 };
 
