d7890e67e66b3a1dd0e60986845fb2a8406d2076
mojpm <itsjoelpm@gmail.com>
2021-02-11 18:25:05 +0300

    add custom hook to fetch data for components that don't need to be mounted

---
+++ src/services/useFetchDataUnmounted.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+export default function useFetchSearchLocationData(url, mockdata) {
+
+    const [data, setData] = useState(mockdata);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchWeatherData() {
+            try {
+                const response = await fetch(url);
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setData(data);
+                } else {
+                    throw response;
+                }
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchWeatherData();
+    }, [url]);
+
+    return { data, error, loading }
+}
