15929425de9140609de4333327081ca8879c8542
mojpm <itsjoelpm@gmail.com>
2021-02-15 21:47:58 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from './components/ErrorBoundary'
 import App from './App'
 
 ReactDOM.render(
