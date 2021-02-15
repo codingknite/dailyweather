094228209ef7873141a3c8e2b6fdc6d8663aa2d4
mojpm <itsjoelpm@gmail.com>
2021-02-15 18:04:12 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ErrorBoundary from './components/ErrorBoundary'
-import App from './App'
+import ErrorBoundary from './components/ErrorBoundary';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
