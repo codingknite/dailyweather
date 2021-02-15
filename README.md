e1ebb5573e0b8a889a0b76e28dd3741685c35412
mojpm <itsjoelpm@gmail.com>
2021-02-15 22:07:01 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Daily Weather
-
-Daily Weather is a weather application LOL :))
-
-## Still In Progress. Coming Soon!
--- src/components/Spinner.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Loader from "react-loader-spinner";
-
-export default function Spinner() {
-  return <Loader type="Rings" height={100} width={100} />;
-}
