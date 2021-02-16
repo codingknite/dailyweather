c9edce468153a66ba8e669b3d72f259c18c0b85a
mojpm <itsjoelpm@gmail.com>
2021-02-16 12:49:11 +0300

    fix network bug

--- src/services/useFetchWeatherData.js
+++ src/services/useFetchWeatherData.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useState, useEffect, useRef } from 'react'
 import MockWeatherData from '../data/MockWeatherData'
 
@@ -8,31 +7,29 @@ export default function useFetchWeatherData(celcius) {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
     const [weatherData, setWeatherData] = useState(MockWeatherData);
-    const apiKey = process.env.REACT_APP_API_KEY;
     const isMounted = useRef(false)
 
 
     useEffect(() => {
-        async function fetchLocation(url) {
-            try {
-                const response = await axios.get(url)
-                const data = await response.data
-                setCity(data.city)
-            } catch (error) {
-                setError(error)
-            }
-        }
-
         async function fetchWeatherData(url) {
             isMounted.current = true;
             try {
-                const response = await axios.get(url);
-                const data = await response.data;
-                if (isMounted.current) setWeatherData(data);
+                const location = await fetch("https://extreme-ip-lookup.com/json/");
+
+                if (location.ok) {
+                    const data = await location.json();
+                    if (isMounted.current) setCity(data.city)
+                    const weather = await fetch(url);
+                    if (weather.ok) {
+                        const data = await weather.json()
+                        if (isMounted.current) setWeatherData(data)
+                    }
+                } else {
+                    throw location
+                }
+
             } catch (e) {
-                if (isMounted.current) {
-                    throw e;
-                };
+                if (isMounted.current) setError(e);
             } finally {
                 if (isMounted.current) setLoading(false);
             }
@@ -42,10 +39,9 @@ export default function useFetchWeatherData(celcius) {
             }
         }
 
-        fetchLocation("https://extreme-ip-lookup.com/json/");
         fetchWeatherData(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0d9a54be6ed79bbc56fec4528ad25e92&units=${celcius ? "metric" : "imperial"}`)
 
-    }, [apiKey, city, celcius])
+    }, [city, celcius])
 
     return { city, weatherData, loading, error }
 }
